ca0b8c7859b78c0f7a38cae90b4e7e1bfd92fbbd
Edward Bustos <esbustosg@eservicios.indracompany.com>
2020-08-24 17:37:10 -0500

    Updates

---
+++ precache-manifest.d12c47e41ff06127917ca469f0760107.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ec97beb94e80ad0f3a12fa8e9fd2b9b",
+    "url": "/crud_react/index.html"
+  },
+  {
+    "revision": "04b69abb564f48999318",
+    "url": "/crud_react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c5c8f88e1ffdd0b71c9b",
+    "url": "/crud_react/static/js/2.98c0dfe3.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/crud_react/static/js/2.98c0dfe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04b69abb564f48999318",
+    "url": "/crud_react/static/js/main.ef60edd7.chunk.js"
+  },
+  {
+    "revision": "315947f1a50d89d9fbe7",
+    "url": "/crud_react/static/js/runtime-main.1c19d6da.js"
+  }
+]);
